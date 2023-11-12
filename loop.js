a3ce7f13a6f67c3c4b64f4b9461a1af5cfb29add
nasuda <nasuda908@gmail.com>
2023-11-12 22:57:13 +0900

    add scripts

---
+++ loop.js
@@ -0,0 +1,16 @@
+module.exports = {
+double: (n) => {
+            for (let i = 1; i <= 10; i = i + 1) {
+                const a = n * i
+                    console.log(a)
+            }
+        },
+f: (n) => {
+  for (i = n; i <= 10; i = i * n) {
+    const a = n * 2
+    console.log(a)
+  }
+}
+
+}
+
