861f192f4e817fe6c378e48b0e7a0c791c8189e1
nasuda <nasuda908@gmail.com>
2023-11-15 22:47:41 +0900

    add import node : readline i am beginner

--- 15/conditions.js
+++ 15/conditions.js
@@ -1,3 +1,17 @@
-if (true) {
-  console.log('conditions');
+import readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({ input, output });
+
+const answer = await rl.question('y or n \n');
+
+if (answer === 'y') {
+    console.log(`Thank you: ${answer}`);
+
+    rl.close();
+} else {
+    console.log('please put "y"')
+    rl.close();
+        
 }
+
