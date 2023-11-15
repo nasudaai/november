6111f7e455d732f5e24f75e200ee12c0e0d8a70e
nasuda <nasuda908@gmail.com>
2023-11-15 22:55:58 +0900

    change import name

--- 15/arr.js
+++ 15/arr.js
@@ -1,7 +1,11 @@
-const arr = [];
+function createArr () {
+    const arr = [];
 
-for (let i = 0; i < 10; i = i +1) {
-  arr.push(i)
+    for (let i = 0; i < 10; i = i +1) {
+        arr.push(i)
+    }
+
+    console.log(arr)
 }
 
-console.log(arr)
+export { createArr };
--- 15/main.js
+++ 15/main.js
@@ -1,8 +1,10 @@
 import { loop } from './loop.js';
+import { createArr as arr } from './arr.js';
 
 function main() {
   console.log('main');
   loop();
+  arr();
 }
 
 
