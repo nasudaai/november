6812e87b4f6bd26afafa3ccb4eb48035f0acbb14
nasuda <nasuda908@gmail.com>
2023-11-16 13:18:59 +0900

    add process array init code

--- 16/node/main.js
+++ 16/node/main.js
@@ -1,2 +1,4 @@
 const { cwd } = require('node:process')
 console.log(`current dirctory: ${cwd()}`)
+
+console.log(process.argv)
--- 16/node/projets/main.js
+++ 16/node/projets/main.js
@@ -2,7 +2,11 @@ import { key } from './lib.cjs';
 console.log(key);
 
 //process
+
 console.log(process.argv);
 
-const arg = process.argv[2];
+const processArgInit = process.argv.length - 1;
+console.log(processArgInit);
+
+const arg = process.argv[processArgInit];
 console.log(`import file is: ${arg}`);
