b557bc4c418b0aeb908308c16a4d355b69320842
nasuda <nasuda908@gmail.com>
2023-11-20 07:35:44 +0900

    add forEach method code

--- twenty/main.js
+++ twenty/main.js
@@ -1,1 +1,23 @@
 console.log('main')
+
+function sum(numbers) {
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++){ 
+    total = total + numbers[i];
+  }
+  return total;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(sum(numbers));
+
+numbers.forEach(currentValue => {
+  console.log(currentValue);
+})
+
+let total = 0;
+numbers.forEach(num => {
+  total = total + num
+})
+
+console.log(`total: ${total}`)
