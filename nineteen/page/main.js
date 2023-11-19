073114e19cb195e1da4a16d0562a5a11acffa919
nasuda <nasuda908@gmail.com>
2023-11-19 21:48:55 +0900

    adjust web page initialize

--- nineteen/page/main.js
+++ nineteen/page/main.js
@@ -1,1 +1,10 @@
 console.log('main')
+
+const root = document.getElementById('root')
+console.log(root)
+
+const test = document.createElement('p')
+test.innerText = 'Test text'
+
+root.appendChild(test)
+
