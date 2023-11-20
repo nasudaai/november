5bb0f063234e2a07f95d39fd71165ac7fb614ff3
nasuda <nasuda908@gmail.com>
2023-11-20 20:21:27 +0900

    add render function

--- twenty/page/main.js
+++ twenty/page/main.js
@@ -1,1 +1,17 @@
-console.log('js')
+//console.log('js')
+//alert('ok')
+
+
+const root = document.getElementById('root')
+
+function render(elm) {
+//
+  root.appendChild(elm)
+}
+
+
+const title = document.createElement('h2')
+title.innerText = 'Title'
+
+
+render(title)
