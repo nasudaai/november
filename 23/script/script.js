d58e50cb885182c9690e273100cd9ddefaf8e8c8
nasuda <nasuda908@gmail.com>
2023-11-23 07:52:08 +0900

    write loop codo

---
+++ 23/script/script.js
@@ -0,0 +1,21 @@
+function forLoop() {
+  console.log('for')
+
+  for (let i = 0; i <= 10; i++) {
+    console.log(i)
+  }
+}
+
+function whileLoop() {
+  console.log('while')
+
+  let j = 0;
+  while( j <= 10 ) {
+    console.log(j)
+
+    j++
+  }
+}
+
+forLoop()
+whileLoop()
